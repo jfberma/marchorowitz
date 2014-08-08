91ae4e942fda2709717c085ab4e02ce78b7e9af0
jberman <jeremy@savevalet.com>
2014-08-08 13:52:56 -0700

    delete

--- djangoroot/project/settings_local.py
+++ djangoroot/project/settings_local.py
@@ -10,7 +10,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'marchorowitz',                    # Or path to database file if using sqlite3.
-        'USER': '',                    # Not used with sqlite3.
+        'USER': 'jeremyberman',                    # Not used with sqlite3.
         'PASSWORD': '',                # Not used with sqlite3.
         'HOST': 'localhost',                    # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                    # Set to empty string for default. Not used with sqlite3.
