b7bd715bffde9a7b01a8800ccf98d4f92fa1b1eb
jberman <jeremy@savevalet.com>
2014-08-08 17:55:08 -0700

    updated script

--- scripts/devUpdate.sh
+++ scripts/devUpdate.sh
@@ -5,9 +5,9 @@ sudo scripts/shutdownSupervisor.sh
 echo "getting latest code"
 git pull
 echo "running update scripts"
-git describe > gitDescribe.txt
-bin/django syncdb --noinput
-bin/django migrate --noinput
-bin/django check_permissions
+#git describe > gitDescribe.txt
+bin/django syncdb
+bin/django migrate
+#bin/django check_permissions
 echo "starting servers"
 sudo bin/supervisord
