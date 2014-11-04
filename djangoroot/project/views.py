e272dc4549a99a96bd178c1afe9f96cd49ebaff9
jberman <jeremy@savevalet.com>
2014-11-03 21:38:20 -0800

    fixed views

--- djangoroot/project/views.py
+++ djangoroot/project/views.py
@@ -1,6 +1,7 @@
 from coin.models import CoinStat
 from django.views.generic import ListView, TemplateView
 from project.models import Piece, PieceCategory
+from shop.models import Order
 
 import logging
 logger = logging.getLogger(__name__)
@@ -21,11 +22,17 @@ class AboutView(TemplateView):
     template_name = "project/about.html"
 
     def get_context_data(self, **kwargs):
-        coin_stats = CoinStat.objects.all()
+        coin_stats = CoinStat.objects.all()[:10]
         context = super(AboutView, self).get_context_data(**kwargs)
-        context['coin_stats'] = coin_stats;
+        context['coin_stats'] = coin_stats
         return context
 
 
 class AccountView(TemplateView):
     template_name = "project/account.html"
+
+    def get_context_data(self, **kwargs):
+        orders = Order.objects.filter(user=self.request.user)
+        context = super(AccountView, self).get_context_data(**kwargs)
+        context['orders'] = orders
+        return context
