7c28056c7d95b6a3a75b0d254ad24587523bba8a
jberman <jeremy@savevalet.com>
2014-11-03 21:37:08 -0800

    coin updates

---
+++ djangoroot/coin/migrations/0007_auto__add_fractionalcoin__add_coinsettings.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'FractionalCoin'
+        db.create_table(u'coin_fractionalcoin', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(related_name='factional_coins', to=orm['auth.User'])),
+            ('amount', self.gf('django.db.models.fields.IntegerField')()),
+        ))
+        db.send_create_signal(u'coin', ['FractionalCoin'])
+
+        # Adding model 'CoinSettings'
+        db.create_table(u'coin_coinsettings', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('mood_weight', self.gf('django.db.models.fields.DecimalField')(default=0.02, max_digits=100, decimal_places=2)),
+            ('productivity_weight', self.gf('django.db.models.fields.DecimalField')(default=0.02, max_digits=100, decimal_places=2)),
+            ('sales_weight', self.gf('django.db.models.fields.DecimalField')(default=0.02, max_digits=100, decimal_places=2)),
+            ('points_per_coin', self.gf('django.db.models.fields.IntegerField')(default=100)),
+        ))
+        db.send_create_signal(u'coin', ['CoinSettings'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'FractionalCoin'
+        db.delete_table(u'coin_fractionalcoin')
+
+        # Deleting model 'CoinSettings'
+        db.delete_table(u'coin_coinsettings')
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'coin.coin': {
+            'Meta': {'object_name': 'Coin'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'coins'", 'to': u"orm['auth.User']"}),
+            'uuid': ('django.db.models.fields.CharField', [], {'max_length': '36', 'blank': 'True'})
+        },
+        u'coin.coinsettings': {
+            'Meta': {'object_name': 'CoinSettings'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'mood_weight': ('django.db.models.fields.DecimalField', [], {'default': '0.02', 'max_digits': '100', 'decimal_places': '2'}),
+            'points_per_coin': ('django.db.models.fields.IntegerField', [], {'default': '100'}),
+            'productivity_weight': ('django.db.models.fields.DecimalField', [], {'default': '0.02', 'max_digits': '100', 'decimal_places': '2'}),
+            'sales_weight': ('django.db.models.fields.DecimalField', [], {'default': '0.02', 'max_digits': '100', 'decimal_places': '2'})
+        },
+        u'coin.coinstat': {
+            'Meta': {'object_name': 'CoinStat'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'mood': ('django.db.models.fields.IntegerField', [], {}),
+            'productivity': ('django.db.models.fields.IntegerField', [], {}),
+            'sales': ('django.db.models.fields.IntegerField', [], {}),
+            'value': ('django.db.models.fields.DecimalField', [], {'max_digits': '1000', 'decimal_places': '2'})
+        },
+        u'coin.fractionalcoin': {
+            'Meta': {'object_name': 'FractionalCoin'},
+            'amount': ('django.db.models.fields.IntegerField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'factional_coins'", 'to': u"orm['auth.User']"})
+        },
+        u'coin.transaction': {
+            'Meta': {'object_name': 'Transaction'},
+            'amount': ('django.db.models.fields.IntegerField', [], {}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'receiver': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'transactions_received'", 'to': u"orm['auth.User']"}),
+            'sender': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'transactions_sent'", 'to': u"orm['auth.User']"})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['coin']
