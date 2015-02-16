92634505a0237b6ba013fc5ea69d22f3d33746b4
jberman <jeremy@savevalet.com>
2015-02-16 11:12:38 -0800

    added cron for coin stats

--- djangoroot/coin/cron.py
+++ djangoroot/coin/cron.py
@@ -4,7 +4,7 @@ from django_cron import CronJobBase, Schedule
 
 
 class UpdateStats(CronJobBase):
-    RUN_EVERY_MINS = 1440
+    RUN_EVERY_MINS = 1439
 
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
     code = 'coin.update_stats'
@@ -13,10 +13,10 @@ class UpdateStats(CronJobBase):
         stat = CoinStat.objects.latest('created')
         now = datetime.datetime.now()
 
-        #if stat.created.day != now.day:
-        new_stat = CoinStat()
-        new_stat.mood = stat.mood
-        new_stat.sales = stat.sales
-        new_stat.productivity = stat.productivity
-        new_stat.value = stat.value
-        new_stat.save()
+        if stat.created.day != now.day:
+            new_stat = CoinStat()
+            new_stat.mood = stat.mood
+            new_stat.sales = stat.sales
+            new_stat.productivity = stat.productivity
+            new_stat.value = stat.value
+            new_stat.save()
